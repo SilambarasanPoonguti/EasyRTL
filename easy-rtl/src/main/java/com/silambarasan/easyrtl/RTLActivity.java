b9ac5ff8f57512ad4e4e065d1ed68de8a8ee4149
SilambarasanPoonguti <imegamechanger@gmail.com>
2016-02-26 11:29:05 +0530

    Initial Commit

---
+++ easy-rtl/src/main/java/com/silambarasan/easyrtl/RTLActivity.java
@@ -0,0 +1,13 @@
+package com.silambarasan.easyrtl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RTLActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rtl);
+    }
+}
